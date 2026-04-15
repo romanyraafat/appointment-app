e8f6fe9df7667767497a6438967888115d205a16
romanyraafat <romanyraafat2004@gmail.com>
2026-04-15 17:35:22 +0200

    create spacing file

---
+++ lib/core/helper/spaceing.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+SizedBox verticalSpacing(int height) => SizedBox(height: height.h,);
+SizedBox horizontalSpacing(int width) => SizedBox(height: width.h,);
